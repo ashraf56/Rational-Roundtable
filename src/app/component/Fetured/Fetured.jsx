d22ec0454e5caf4f99131e1f84a0b55f707d998d
ashraf56 <ashrafulfahim07@gmail.com>
2023-11-24 12:00:34 +0600

    Featured section  updated

--- src/app/component/Fetured/Fetured.jsx
+++ src/app/component/Fetured/Fetured.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 import img from '../../../asset/claus-grunstaudl-dKeB0-M9iiA-unsplash.jpg'
 import { FaPlayCircle } from 'react-icons/fa';
 const Fetured = () => {
+    const imgs='https://th.bing.com/th/id/OIG.fgd5p7JxyQQ6v_y1pLDV?w=1024&h=1024&rs=1&pid=ImgDetMain'
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row">
-  <Image src={img} alt='f' width={800} height={300} className='rounded-s-full rounded-t-full max-w-full'></Image>   
+  <Image src={imgs} alt='f' width={500} height={600} className='rounded-s-full rounded-t-full max-w-full'></Image>   
    <div>
       <p className='bg-clip-text text-transparent bg-gradient-to-r from-[#ebf4f5] to-[#d3f3f1] '>Featured Epidsode</p>
       <h1 className=" text-base md:text-5xl font-bold">Catalyst Conversations: Igniting Change through Insight</h1>
