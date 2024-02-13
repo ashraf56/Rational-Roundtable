ac02c7ad36ea1325df32657e0ce9f797241b1ad4
ashraf56 <ashrafulfahim07@gmail.com>
2024-02-13 16:20:59 +0600

    image rendaring  problem solved

--- src/app/component/Recent/Recent.jsx
+++ src/app/component/Recent/Recent.jsx
@@ -10,7 +10,7 @@ const episodes = [
     duration: "30:00",
     releaseDate: "22 November 2023",
     host: "John Doe",
-    image: "https://th.bing.com/th/id/OIG.teLGSyfW9Hh9oMSiGRSE?pid=ImgGn",
+    image: "https://images.unsplash.com/photo-1532971291849-92617bf94ded?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     episodeNumber: 2,
@@ -19,7 +19,7 @@ const episodes = [
     duration: "25:00",
     releaseDate: "29 November 2023",
     host: "John Doe",
-    image: "https://th.bing.com/th/id/OIG.35bt63SOkIwTYAMEb_iy?pid=ImgGn",
+    image: "https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     episodeNumber: 3,
@@ -37,7 +37,7 @@ const episodes = [
     duration: "32:00",
     releaseDate: "13 December 2023",
     host: "John Doe",
-    image: "https://th.bing.com/th/id/OIG.35bt63SOkIwTYAMEb_iy?pid=ImgGn",
+    image: "https://images.unsplash.com/photo-1485955891060-a3318433e95f?q=80&w=1370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     episodeNumber: 5,
@@ -55,7 +55,7 @@ const episodes = [
     duration: "31:00",
     releaseDate: "27 December 2023",
     host: "John Doe",
-    image: "https://th.bing.com/th/id/OIG.daEMkeE28PnOflfVYu.T?pid=ImgGn",
+    image: "https://images.unsplash.com/photo-1616627981222-89035e5f5242?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
@@ -69,14 +69,14 @@ const Recent = () => {
       <h1 className=" text-base md:text-5xl font-bold uppercase text-center  text-white">Recent Episodes</h1>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-2 justify-center items-center mx-auto py-10 px-10'>
         {
-          episodes.map(p => (
-            <div className="card max-w-full h-full bg-base-100 shadow-xl text-white " key={p.title} style={{ backgroundImage: ` linear-gradient(to right , #111111 0%, rgba(17, 17, 17, 0) 100%),url(${p.image})`, backgroundPosition: 'center', backgroundSize: 'cover', height: '100%', }}>
+          episodes?.map(p => (
+            <div className="card max-w-full h-full bg-base-100 shadow-xl text-white " key={p.title} style={{ backgroundImage: ` linear-gradient(to right , #111111 0%, rgba(17, 17, 17, 0) 100%),url(${p?.image})`, backgroundPosition: 'center', backgroundSize: 'cover', height: '100%',objectFit:'contain' }}>
               <div className="card-body">
                 <p> Episode {p.episodeNumber}</p>
                 <p className='font-bold'> {p.host}</p>
                 <h2 className="card-title">{p.title}</h2>
                 <p>{p.description}</p>
-                <p className='flex  '><span className='px-1'>{p.duration}</span> h</p>
+                <p className='flex  '><span className='px-1'>{p.duration}</span> min</p>
 
                 <p className='font-bold'>{p.releaseDate}</p>
                 <div className="card-actions justify-end">
