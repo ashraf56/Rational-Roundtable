4430115581ef641f54e672c94d9baa4fcc9a36c6
ashraf56 <ashrafulfahim07@gmail.com>
2024-02-13 17:18:01 +0600

    image rendaring  problem solved

--- src/app/component/Fetured/Fetured.jsx
+++ src/app/component/Fetured/Fetured.jsx
@@ -1,14 +1,13 @@
 import Image from 'next/image';
 import React from 'react';
-import img from '../../../asset/claus-grunstaudl-dKeB0-M9iiA-unsplash.jpg'
+import img from '../../../asset/ezgif-6-6e8f73717d.jpg'
 import { FaPlayCircle } from 'react-icons/fa';
 const Fetured = () => {
-    const imgs = 'https://th.bing.com/th/id/OIG.fgd5p7JxyQQ6v_y1pLDV?w=1024&h=1024&rs=1&pid=ImgDetMain'
     return (
         <div>
             <div className="hero min-h-screen bg-base-200 text-white">
                 <div className="hero-content flex-col lg:flex-row">
-                    <Image src={imgs} alt='f' width={500} height={600} className='rounded-s-full rounded-t-full max-w-full'></Image>
+                    <Image src={img} alt='f' width={500} height={600} className='rounded-s-full rounded-t-full max-w-full'></Image>
                     <div>
                         <p className='bg-clip-text text-transparent bg-gradient-to-r from-[#ebf4f5] to-[#d3f3f1] '>Featured Epidsode</p>
                         <h1 className=" text-base md:text-5xl font-bold">Catalyst Conversations: Igniting Change through Insight</h1>
